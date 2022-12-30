36a49af36e267e320732e23a69236094ecd53d00
Artur <arturcentner@gmail.com>
2022-12-31 00:24:34 +0100

    начал делать web-api

---
+++ WebAccounting/Controllers/ComputerController.cs
@@ -0,0 +1,77 @@
+﻿using DBLibrary;
+using DBLibrary.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+
+namespace WebAccounting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputerController : ControllerBase
+    {
+        private readonly ComputerService _service;
+
+        public ComputerController()
+        {
+            var context = new DbConnect();
+            _service = new ComputerService(context);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<IList<Computer>> GetComputers()
+        {
+            var result = _service.GetComputers(10,0);
+            if (result.Count == 0) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetByID")]
+        public ActionResult<Computer> GetByID(uint id)
+        {
+            var result = _service.GetByID(id);
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("Filters")]
+        public ActionResult<IList<Computer>> Filters(string? nameFilter,
+            string? priceFilter, int statusFilter, string? emplID)
+        {
+            var result = _service.GetComputers(10, 0, nameFilter, priceFilter, statusFilter, emplID);
+            if(result.Count == 0) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        //по идее для компьютера тоже нужен свой респонс
+        public ActionResult Create(Computer computer)
+        {
+            _service.Create(computer);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Change")]
+        public ActionResult Update(Computer computer)
+        {
+            _service.Update(computer);
+            return Ok(computer);
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult Delete(uint id)
+        {
+            _service.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ WebAccounting/Controllers/EmploeeController.cs
@@ -0,0 +1,77 @@
+﻿using DBLibrary;
+using DBLibrary.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+
+namespace WebAccounting.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmploeeController : ControllerBase
+{
+    private readonly EmployeeService _service;
+    
+    public EmploeeController()
+    {
+        var context = new DbConnect();
+        _service = new EmployeeService(context);
+    }
+
+    [HttpGet]
+    [Route("GetAll")]
+    public ActionResult<IList<Employee>> GetEmployees()
+    {
+        var result = _service.GetEmployees(10, 0);
+        if (result.Count == 0) return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Route("GetByID")]
+    public ActionResult<Employee> GetByID(uint id)
+    {
+        var result = _service.GetByID(id);
+        if (result == null) return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("CreateEmployee")]
+    public ActionResult Registration(Employee employee)
+    {
+        var result = _service.Registration(employee);
+        if (!result.IsSuccess) return BadRequest(result.Message);
+
+        return Ok(result.Message);
+    }
+
+    [HttpPut]
+    [Route("ChangeEmployee")]
+    public ActionResult Update(Employee employee)
+    {
+        var result = _service.Update(employee);
+        if (!result.IsSuccess) return BadRequest(result.Message);
+        return Ok(result.Message);
+    }
+
+    [HttpGet]
+    [Route("Login")]
+    public ActionResult<Employee> Login(string login, string password)
+    {
+        var result = _service.Login(login, password);
+        if (!result.IsSuccess) return BadRequest(result.Message);
+
+        return Ok(result.Employee);
+    }
+
+    [HttpDelete]
+    [Route("Delete")]
+    //сделать сервис.делит возвращаемым эмплойереспонс
+    public ActionResult Delete(uint id)
+    {
+        _service.Delete(id);
+        return Ok();
+    }
+}
